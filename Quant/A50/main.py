d093f5f4e55d2279827735ead75c77a4ee2f11b3
unknown <knowckx@foxmail.com>
2019-08-21 00:37:58 +0800

    upup

--- Quant/A50/main.py
+++ Quant/A50/main.py
@@ -5,13 +5,15 @@ import datetime
 
 
 def test():
-    tarFile = '000925.XSHG_5m'
+    # tarFile = '000925.XSHG_5m'
+    tarFile = '000925.XSHG_1d'
+
     df = csv.GetPDdata(tarFile)  # data prepare
 
     # print(df[-1:])  # test DF
 
     # startData = "2013-12-27 00:00:00"
-    startData = "2019-08-02 15:00:00"
+    startData = "2019-01-01 00:00:00"
     # startData = "2019-08-09 14:50:00"
 
 
@@ -21,19 +23,21 @@ def test():
     while idx < df.index[-1]:
         dftest = df.loc[idx-200:idx]
         # print(dftest[-1:])
-        dvg.Start(dftest,"5m")
+        dvg.Start(dftest,"1d")
         idx +=1
 
-FormatDateTime = "%Y-%m-%d %H:%M:%S"
+FormatDateTime1 = "%Y-%m-%d"
+FormatDateTime2 = "%Y-%m-%d %H:%M:%S"
+
 
 # 在df里找到对应的时间index
 def GetDateIndex(df, date):
     timeL = df.time
-    date = datetime.datetime.strptime(date, FormatDateTime)
+    date = datetime.datetime.strptime(date, FormatDateTime2)
     i = df.index[-1] #最后一个索引
     while i > 0: 
         strtime = timeL.at[i] #从csv读出来是字符串
-        dftime = datetime.datetime.strptime(strtime, FormatDateTime)
+        dftime = datetime.datetime.strptime(strtime, FormatDateTime1)
         if dftime <= date:
             return i
         i -= 1
